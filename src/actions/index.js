47208abde38ee00f74f7cecfc8d7dfc72a3c0896
Han <hanshengzhao1993@gmail.com>
2018-04-25 23:39:31 -0700

    added fetch weather function

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '191e48012edee30f79ebf1b9fa734796';
+
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
